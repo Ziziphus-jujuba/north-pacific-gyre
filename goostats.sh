c7506e2acfb2c0b2f1b7a07a547dc3a6d61884b3
= <teresahy94@gmail.com>
2024-09-06 11:56:36 +0800

    use supplied filename

--- goostats.sh
+++ goostats.sh
@@ -1,7 +1,7 @@
 # Load a given file
 # Computer the min/max/range f values in a file
 fname=$1
-echo"working with file:"
+echo"working with file: ${fname}"
 echo "Welcome scrt"
 # compute min/max/range of values in a file
 
